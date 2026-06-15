0c3d38cb45ec633a926508e162e5c092f34f2de2
Suraag Baniya <suraag.baniya.sb@gmail.com>
2026-06-15 18:43:26 +0545

    register, login, logout useraccounts etcs

---
+++ Chapter19/LearningLog/users/urls.py
@@ -0,0 +1,22 @@
+""" Define URL patterns for users """
+
+from django.urls import path
+from django.contrib.auth.views import LoginView 
+
+from . import views
+
+urlpatterns = [
+    # Login page
+    # path('login/', LoginView.as_view(), {'template_name': 'users/login.html'}, name= 'login' ),
+    
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    
+    #Logout page
+    
+    path('logout/', views.logout_view, name='logout'),
+    
+    #Registration page
+    
+    path('register/', views.register, name='register'),
+
+]
